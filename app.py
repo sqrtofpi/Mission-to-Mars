a45f63e5cdd81217f33fc475cba17030a84c2a46
imaMacDonald <macsdontquit@icloud.com>
2021-08-07 17:05:42 -0400

    Uploading Challenge Files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scraping
 
@@ -18,7 +18,7 @@ def scrape():
    mars = mongo.db.mars
    mars_data = scraping.scrape_all()
    mars.update({},mars_data, upsert=True)
-   return "Scraping Successful!"
+   return redirect("/", code=302)
 
 if __name__ == "__main__":
    app.run(debug=True)
